0e2c7332ead7882999713a18afc961597dbfe7c3
Victor <victor.pacheco@case-mate.com>
2017-02-08 10:18:58 -0500

    adding dotenv to keep app settings

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+require('dotenv').config();
 var app = express();
 
 // view engine setup
--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -1,27 +1,25 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
+
 //var config = require('../config/config.json'); //testing
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-	console.log(process.env.appId);
   res.render('index', { title: 'Express' });
-
 });
 
 router.get('/install',function(req,res,next){
 	var shop  = req.query.shop;
 	console.log(shop);
-	var appId=1;
-	var appSecret=2;
-	var appScope='read_orders';
-	var appDomain='af.com';
+	var appId=process.env.appId;
+	var appSecret=process.env.appSecret;
+	var appScope=process.env.appScope;
+	var appDomain=process.env.appDomain;
+
 	//build the url
 	var url = `https://${shop}/admin/oauth/authorize?client_id=${appId}&scope=${appScope}&redirect_uri=https://${appDomain}/auth`;
-	//res.redirect('');
+	
 	console.log(url);
-	res.send('testing');
+	res.redirect(url);
 });
 
 module.exports = router;
