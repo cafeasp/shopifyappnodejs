87f254d81644d5c9d274b1dab1a6b0b918914c63
Victor Pacheco <vpacheco99@gmail.com>
2017-02-08 11:12:59 -0500

    install call works

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/install',function(req,res,next){
+router.get('/shopify/install',function(req,res,next){
 	var shop  = req.query.shop;
 	console.log(shop);
 	var appId=process.env.appId;
@@ -16,10 +16,16 @@ router.get('/install',function(req,res,next){
 	var appDomain=process.env.appDomain;
 
 	//build the url
-	var url = `https://${shop}/admin/oauth/authorize?client_id=${appId}&scope=${appScope}&redirect_uri=https://${appDomain}/auth`;
+	var url = `https://${shop}/admin/oauth/authorize?client_id=${appId}&scope=${appScope}&redirect_uri=https://${appDomain}/shopify/auth`;
 	
 	console.log(url);
 	res.redirect(url);
+});
+
+router.get('/shopify/auth',function(req,res,next){
+	//todo 
+	//get token
+	res.render('index',{title:'get token'});
 });
 
 module.exports = router;
