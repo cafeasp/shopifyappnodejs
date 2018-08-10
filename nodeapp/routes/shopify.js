4eb13b5bf76a5da2550abe377bca06a0e8906351
Andrew McDonald <maccyd10@gmail.com>
2018-08-10 09:15:26 -0700

    'securityPass' variable being over written and allowing failed regex to pass -- Fixed

--- nodeapp/routes/shopify.js
+++ nodeapp/routes/shopify.js
@@ -19,7 +19,7 @@ router.get('/install', function (req, res, next) {
 });
 
 router.get('/auth', function (req, res, next) {
-    let securityPass = false;
+    let validRegex = false;
     let shop = req.query.shop;
     let code = req.query.code;
 
@@ -28,10 +28,10 @@ router.get('/auth', function (req, res, next) {
 
     if (shop.match(regex)) {
         console.log('regex is ok');
-        securityPass = true;
+        validRegex = true;
     } else {
         //exit
-        securityPass = false;
+        validRegex = false;
     }
 
     // 1. Parse the string URL to object
@@ -48,7 +48,7 @@ router.get('/auth', function (req, res, next) {
     }
     let appId = process.env.appId;
     let appSecret = process.env.appSecret;
-    if (securityPass) {
+    if (validRegex && securityPass) {
 
         //Exchange temporary code for a permanent access token
         let accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
